82166e95892d2e995643c5db6beedbbde7b6ebca
mariocodev <mariocodev@gmail.com>
2024-10-07 23:50:40 -0300

    feat: add icon and formSearch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
 import { Character } from './shared/models/character.interface';
 import { DragonBallService } from './shared/services/dragon-ball.service';
+import { ApiResponse } from './shared/models/api-response.interface';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, CommonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: 'app-root',
+	standalone: true,
+	imports: [RouterOutlet, CommonModule, ReactiveFormsModule],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  characters: Character[] = [];
-  constructor(private __dragonBallService: DragonBallService) {}
+	characters: Character[] = [];
+	searchForm: FormGroup;
 
-  ngOnInit(): void {
-    this.__dragonBallService.getCharacters().subscribe({
-      next: (response) => {
-        this.characters = response.items;
-      },
-      error: (error) => {
-        console.error('Error fetching characters:', error);
-      },
-    });
-  }
+	constructor(
+		private __dragonBallService: DragonBallService,
+		private fb: FormBuilder
+	) {
+		// Inicializa el formulario
+		this.searchForm = this.fb.group({
+			name: [''],
+			gender: [''],
+			race: [''],
+			affiliation: [''],
+			sort: ['']
+		});
+	}
+
+	ngOnInit(): void {
+		//this.loadCharacters(0, 1, 15, 'Go', 'Male', 'Saiyan', 'Z Fighter');
+		this.loadCharacters(0, 1, 15);
+	}
+
+	loadCharacters(start: number, page: number, limit: number/*, name?: string, gender?: string, race?: string, affiliation?: string*/) {
+		const { name, gender, race, affiliation } = this.searchForm.value;
+		this.__dragonBallService.getCharacters(start, page, limit, name, gender, race, affiliation)
+			.subscribe((response: ApiResponse) => {
+				this.characters = response.items ?? response;
+				//console.log(response);
+			});
+	}
+
+	onSearch() {
+		this.loadCharacters(0, 1, 15); // Reinicia la búsqueda al primer conjunto de resultados
+	}
 }
--- src/app/shared/services/dragon-ball.service.ts
+++ src/app/shared/services/dragon-ball.service.ts
@@ -1,18 +1,43 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ApiResponse } from '../models/api-response.interface';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  	providedIn: 'root'
 })
 export class DragonBallService {
 
-  private apiUrl = 'https://dragonball-api.com/api/characters';
+	private apiUrl = 'https://dragonball-api.com/api/characters';
 
-  constructor(private http: HttpClient) {}
+	constructor(private http: HttpClient) {}
 
-  getCharacters(): Observable<ApiResponse> {
-    return this.http.get<ApiResponse>(this.apiUrl);
-  }
+	//'https://dragonball-api.com/api/characters?page=1&limit=15&name=a&gender=Male&race=Saiyan&affiliation=Z%20Fighter'
+	getCharacters(
+		start: number,
+		page: number,
+		limit: number,
+		name?: string,
+		gender?: string,
+		race?: string,
+		affiliation?: string
+	): Observable<ApiResponse> {
+
+		let params = new HttpParams()
+		.set('page', `${Math.ceil(start / limit)}`) // calcular la página correctamente
+		.set('limit', `${limit}`);
+
+		if (name) params = params.set('name', name);
+		if (gender) params = params.set('gender', gender);
+		if (race) params = params.set('race', race);
+		if (affiliation) params = params.set('affiliation', affiliation);
+
+		//console.log(params.toString()); // Para depuración
+
+		// Imprimir la URL completa
+		const urlWithParams = `${this.apiUrl}?${params.toString()}`;
+		console.log('URL Consultada:', urlWithParams);
+
+		return this.http.get<ApiResponse>(this.apiUrl, { params });
+	}
 }
