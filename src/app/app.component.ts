cc9302cd2b87c93b31ffa33074a8874a89616646
mariocodev <mariocodev@gmail.com>
2024-10-09 00:45:43 -0300

    feat: add tailwind
    Delete Bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormArray, ReactiveFormsModule } from '@angular/forms';
 
 import { Character } from './shared/models/character.interface';
 import { DragonBallService } from './shared/services/dragon-ball.service';
 import { ApiResponse } from './shared/models/api-response.interface';
 import { environment } from '../environments/environment';
+import { AfilliationData, FilterResponse, GenderData, RaceData } from './shared/models/data-filter.model';
 
 @Component({
 	selector: 'app-root',
@@ -15,10 +16,14 @@ import { environment } from '../environments/environment';
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent  implements OnInit {
+
 	characters: Character[] = [];
 	searchForm: FormGroup;
-	countCharacter: number = 0;
+	genders: FilterResponse[] = GenderData;
+	races: FilterResponse[] = RaceData;
+	affiliations: FilterResponse[] = AfilliationData;
+	loading: boolean = false;
 
 	ENV: boolean = environment.production;
 
@@ -39,31 +44,53 @@ export class AppComponent {
 	}
 
 	ngOnInit(): void {
-		//this.loadCharacters(0, 1, 15, 'Go', 'Male', 'Saiyan', 'Z Fighter');
-		//this.loadCharacters(0, 1, 15);
 		this.onSearch();
+		this.createGenderOptions();
+		this.createRaceOptions();
+		this.createAffiliationsOptions();
 	}
 
 	loadCharacters(start: number, page: number, limit: number/*, name?: string, gender?: string, race?: string, affiliation?: string*/) {
+		this.loading = true;
 		const { name, gender, race, affiliation } = this.searchForm.value;
 		this.__dragonBallService.getCharacters(start, page, limit, name, gender, race, affiliation)
 			.subscribe((response: ApiResponse) => {
 				this.characters = response.items ?? response;
+				this.loading = false
 				//console.log(response);
 			});
 	}
 
-	countCharacters(limit: number): void {
-		this.__dragonBallService.getCharacters(0, 1, limit)
-			.subscribe((response: ApiResponse) => {this.countCharacter = response.items.length;},
-            (error) => {
-                console.error('Error al obtener el conteo de personajes:', error);
-                this.countCharacter = 0;
-            });
+	// Formulario
+	createGenderOptions() {
+		this.genders.forEach(gender => {
+		  	this.searchForm.addControl(gender.value, this.fb.control(false));
+		});
+	}
+
+	createRaceOptions() {	
+		this.races.forEach(race => {
+		  	this.searchForm.addControl(race.value, this.fb.control(false));
+		});
+	}
+
+	createAffiliationsOptions() {	
+		this.affiliations.forEach(affiliation => {
+		  	this.searchForm.addControl(affiliation.value, this.fb.control(false));
+		});
 	}
 
 	onSearch() {
+
 		this.loadCharacters(0, 1, this.LIMIT_RESULT); // Reinicia la búsqueda al primer conjunto de resultados
-		this.countCharacters(100);
+	}
+	
+	resetForm() {
+		this.searchForm.reset(); // Resetea todos los campos del formulario
+	}
+
+	resetFormFieldsetGender(): void {
+		this.searchForm.reset();
+		//this.onSearch();
 	}
 }
---
+++ src/app/shared/models/data-filter.model.ts
@@ -0,0 +1,38 @@
+export interface FilterResponse {
+    value:  string;
+    label:  string;
+}
+
+export const GenderData = [
+    { value: 'Male', label: 'Hombre'},
+    { value: 'Female', label: 'Mujer'}
+];
+
+export const RaceData = [
+    { value: 'Saiyan', label: 'Saiyan'},
+    { value: 'Namekian', label: 'Namekian'},
+    { value: 'Human', label: 'Humano'},
+    { value: 'Majin', label: 'Majin'},
+    { value: 'Frieza Race', label: 'Frieza Race'},
+    { value: 'Jiren Race', label: 'Jiren Race'},
+    { value: 'Android', label: 'Android'},
+    { value: 'God', label: 'God'},
+    { value: 'Angel', label: 'Angel'},
+    { value: 'Evil', label: 'Evil'},
+    { value: 'Unknown', label: 'Unknown'},
+    { value: 'Nucleico benigno', label: 'Nucleico benigno'},
+
+];
+
+export const AfilliationData = [
+    { value: 'Z Fighter', label: 'Guerreros Z'},
+    { value: 'Red Ribbon Army', label: 'Patrulla Roja'},
+    { value: 'Namekian Warrior', label: 'Namekian Warrior'},
+    { value: 'Freelancer', label: 'Freelancer'},
+    { value: 'Army of Frieza', label: 'Army of Frieza'},
+    { value: 'Other', label: 'Other'},
+    { value: 'Pride Troopers', label: 'Pride Troopers'},
+    { value: 'Assistant of Vermoud', label: 'Assistant of Vermoud'},
+    { value: 'Assistant of Beerus', label: 'Assistant of Beerus'},
+    { value: 'Villain', label: 'Villain'},
+];
